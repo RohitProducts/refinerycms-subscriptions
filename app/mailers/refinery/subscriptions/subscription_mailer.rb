9bf70f290b89af8e8e10a08916d3d3c3c795b66b
Ivan Lučev <lucev.ivan@gmail.com>
2013-10-02 13:18:05 +0200

    Add subscription mailer

---
+++ app/mailers/refinery/subscriptions/subscription_mailer.rb
@@ -0,0 +1,17 @@
+module Refinery
+  module Subscriptions
+    class SubscriptionMailer < ActionMailer::Base
+
+      def notification(subscription, request)
+        @subscription = subscription
+        mail  :subject => Refinery::Subscriptions::Setting.notification_subject
+              :to => Refinery::Subscriptions::Setting.notification_recipients,
+              :from => ::I18n.t('from_name',
+                                :scope => 'refinery.subscriptions.config',
+                                :site_name => Refinery::Core.site_name,
+                                :name => "#{Refinery::Subscriptions.from_name}@#{request.domain}",
+                                :reply_to => @subscription.email
+      end
+    end
+  end
+end
